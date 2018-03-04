208ebf129de9942a0aff640944385e6c09213621
Ryan Hunter <rhunter4@xula.edu>
2018-03-04 13:38:56 -0600

    // Output tomorrow's date using a formatter.

--- src/Main.java
+++ src/Main.java
@@ -10,11 +10,15 @@ public class Main {
         // Output the current date-time.
         LocalDateTime currentDate = currentDateTime();
         System.out.println("Current Date and Time: " + currentDate);
+
         // Output tomorrow's date using a formatter.
         String tomorrowDate = tomorrowDate();
         System.out.println("Tomorrow's Date: " + tomorrowDate);
 
         //TODO Add 5 weeks to today's LocalDateTime.
+        LocalDateTime weeksAdded = weeksAddedToCurrent(currentDate);
+        System.out.println("Five Weeks From " + currentDate + ":\t" + weeksAdded);
+
         //TODO Initialize a LocalDateTime object to your birth date and the time 12:35 PM.
         //TODO Output the day of the week (Sunday-Saturday) that you were born.
         //TODO Output the number of days you've been alive.
@@ -35,6 +39,10 @@ public class Main {
         //TODO
 
 
+    }
+
+    private static LocalDateTime weeksAddedToCurrent(LocalDateTime currentDate) {
+        return currentDate.plusWeeks(5);
     }
 
     private static String tomorrowDate() {
