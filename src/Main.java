da23a03698da8d64e9eb59b25f44d97d6defef04
Ryan Hunter <rhunter4@xula.edu>
2018-03-04 09:27:32 -0600

    // Output the current date-time.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,38 @@
+import java.time.LocalDateTime;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("\n\nHello, AssignmentsApp!\n");
+
+        // Output the current date-time.
+        LocalDateTime currentDate = currentDateTime();
+        System.out.println("Current Date and Time: " + currentDate);
+        //TODO Output tomorrow's date using a formatter.
+        //TODO Add 5 weeks to today's LocalDateTime.
+        //TODO Initialize a LocalDateTime object to your birthdate and the time 12:35 PM.
+        //TODO Output the day of the week (Sunday-Saturday) that you were born.
+        //TODO Output the number of days you've been alive.
+        //TODO Output the number of days between two dates.
+        //TODO
+        //TODO
+        //TODO
+        //TODO
+        //TODO
+        //TODO
+        //TODO
+        //TODO
+        //TODO
+        //TODO
+        //TODO
+        //TODO
+        //TODO
+        //TODO
+
+
+    }
+
+    private static LocalDateTime currentDateTime() {
+        return LocalDateTime.now();
     }
 }
